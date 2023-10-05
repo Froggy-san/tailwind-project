a26e9bd21a565172387cac25bfa9142806a38370
Froggy-san <132022099+Froggy-san@users.noreply.github.com>
2023-10-05 08:46:07 +0300

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+console.log("we are here");
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+console.log(buttons);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+
+    console.log(newIndex, "here");
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    console.log(newIndex);
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
+
+const test = document.getElementById("here-1");
+console.log(test);
